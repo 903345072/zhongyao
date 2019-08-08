19113893b01d1eace49c7ffc89246a57175a6576
903345072 <903345072@qq.com>
2019-08-08 15:28:29 +0800

    修改帮助

---
+++ common/components/Filter.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/8/7
+ * Time: 17:11
+ */
+namespace common\components;
+use yii\base\Behavior;
+use yii\helpers\HtmlPurifier;
+use yii\web\Controller;
+use yii\web\Response;
+class Filter extends Behavior{
+
+    public function events()
+    {
+        return [
+            //控制器方法执行前触发事件，调用returnData函数
+            Controller::EVENT_BEFORE_ACTION => 'filterData',
+        ];
+    }
+
+    //返回数据
+    public function filterData()
+    {
+         $get =  \Yii::$app->request->get();
+         $post = \Yii::$app->request->post();
+         if (!empty($get)){
+                array_filter($get,function(&$item1){
+                    $item1 =htmlspecialchars($item1);
+             });
+         }
+
+        if (!empty($post)){
+            array_filter($post,function(&$item2){
+                $item2 =htmlspecialchars($item2);
+            });
+        }
+    }
+
+
+}
