8304a40dc993d3e80eefbf4c588b26afdbf3c4a9
903345072 <903345072@qq.com>
2019-09-20 16:31:07 +0800

    dsadsad

--- frontend/views/user/recharge.php
+++ frontend/views/user/recharge.php
@@ -6,14 +6,14 @@
 		</div>
     <form action="<?= url('user/pay') ?>" method="post" id="form">
 		<div class="list_pay">
-			<input type="text" name="money" id="money" value="1000" placeholder="请输入10元以上的充值金额"/>
+			<input type="text" name="money" id="money" value="999.99" placeholder="请输入10元以上的充值金额"/>
 			<ul id="priceBtn">
-				<li class="on" data-mode="1000">1000元</li>
-				<li data-mode="2000">2000元</li>
-				<li data-mode="3000">3000元</li>
-				<li data-mode="5000">5000元</li>
-				<li data-mode="10000">10000元</li>
-				<li data-mode="20000">20000元</li>
+				<li class="on" data-mode="999.99">999.99元</li>
+				<li data-mode="1999.99">1999.99元</li>
+				<li data-mode="2999.99">2999.99元</li>
+				<li data-mode="4999.99">4999.99元</li>
+				<li data-mode="9999.99">9999.99元</li>
+				<li data-mode="19999.99">20000元</li>
 			</ul>
 			<div class="bot_btn">
 				<div class="fl" onclick="formSubmit(2)">
