dd6cae9dd6661aee550c9d67dc34812065188f1f
903345072 <903345072@qq.com>
2019-11-05 16:09:08 +0800

    321213

--- frontend/views/user/rechargeList.php
+++ frontend/views/user/rechargeList.php
@@ -3,7 +3,7 @@
   <img src="/wap/img/icons_03.png" onclick="javascript:history.back()">
 </div>
 <?php
-$type = [1=>'支付宝',2=>'微信',3=>'银联',4=>'支付宝'];
+$type = [1=>'支付宝',2=>'微信',3=>'微信',4=>'支付宝'];
 ?>
 <div class="tables">
   <div class="tab_top">
